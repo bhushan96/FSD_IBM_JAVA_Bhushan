5b9b95d40fcbb701bc279fc98a9063723ad153c4
bhushan96 <47738654+bhushan96@users.noreply.github.com>
2019-10-10 17:32:21 +0530

    Add files via upload

--- 10th Oct 2019/APIDemo.java
+++ 10th Oct 2019/APIDemo.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Comparator;
+import java.util.Collections;
 class Person{
 	private String name;
 	private Integer id;
@@ -27,8 +29,67 @@ class Person{
 	public String toString(){
 		return "Id : "+this.getId()+" Name : "+this.getName()+" ,Address : "+this.getAddress();
 	}
+
+	public String getFirstName(){
+		String name2[]=this.getName().split(" ");
+		return name2[0];
+		
+	}
+
+	public String getLastName(){
+
+		String name2[]=this.getName().split(" ");
+		int len=name2.length;
+		if(len>1){
+		return name2[len-1];
+		}
+		else{
+			return "";
+		}
+		
+	}
+
 }
 
+
+class SortById implements Comparator{
+	@Override
+	public int compare(Object ob1,Object ob2){
+
+		return ((Person)ob1).getId().compareTo(((Person)ob2).getId());
+	}
+
+}
+
+class SortByAddress implements Comparator{
+	@Override
+	public int compare(Object ob1,Object ob2){
+
+		return ((Person)ob1).getAddress().compareTo(((Person)ob2).getAddress());
+	}
+
+}
+
+class SortByFirstName implements Comparator{
+
+
+
+	@Override
+	public int compare(Object ob1,Object ob2){
+
+		return ((Person)ob1).getFirstName().compareTo(((Person)ob2).getFirstName());
+	}
+
+}
+
+class SortByLastName implements Comparator{
+	@Override
+	public int compare(Object ob1,Object ob2){
+
+		return ((Person)ob1).getLastName().compareTo(((Person)ob2).getLastName());
+	}
+
+}
 class APIDemo{
 	public static void main(String[] args) {
 		String name="",address="";
@@ -46,9 +107,10 @@ class APIDemo{
 		System.out.println("2. Display all details");
 		System.out.println("3. Search the data");
 		System.out.println("4. Delete data based on id");
-		System.out.println("5. Exit");
+		System.out.println("5. Sort the data");
+		System.out.println("6. Exit");
 
-		System.out.println("Enter your choice (1/2/3/4/5):");
+		System.out.println("Enter your choice (1/2/3/4/5/6):");
 		choice=scan.nextInt();
 		scan.nextLine();
 
@@ -100,7 +162,7 @@ class APIDemo{
 
 				for(Object ob:list){
 				//Person p=new Person();
-				
+
 
 				if(((Person)ob).getId().equals(id_search)){
 					System.out.println(((Person)ob));
@@ -162,13 +224,55 @@ class APIDemo{
 			}
 			break;
 
+		case 5:
+
+			System.out.print("\n");
+			System.out.println("Sort based on-");
+			System.out.println("1. Id");
+			System.out.println("2. Address");
+			System.out.println("3. First Name");
+			System.out.println("4. Last Name");
+			System.out.println("Enter your choice(1/2/3/4)");
+			int choice3=scan.nextInt();
+			scan.nextLine();
+
+			switch(choice3){
+				case 1:
+
+					Collections.sort(list,new SortById());
+
+					break;
+
+				case 2:
+
+					Collections.sort(list,new SortByAddress());
+
+					break;
+
+				case 3:
+
+					Collections.sort(list,new SortByFirstName());
+
+					break;
+
+				case 4:
+
+					Collections.sort(list,new SortByLastName());
+
+					break;
+
+
+
+			}
+
+
 
 
 
 
 	}
 
-	}while(choice<5 && choice>0);
+	}while(choice<6 && choice>0);
 
 
 
